5806c7f6369443fd62b8f027305c1399dee22620
Steven <srothwell4@gmail.com>
2020-04-24 19:58:07 +0100

    import existing route53 zone and NS and SOA records. Create new A record

---
+++ dns.tf
@@ -0,0 +1,38 @@
+resource "aws_route53_zone" "rothwell-dev-zone" {
+  name = "rothwell.dev"
+}
+
+resource "aws_route53_record" "rothwell-dev-ns" {
+  zone_id = aws_route53_zone.rothwell-dev-zone.zone_id
+  name    = "rothwell.dev"
+  type    = "NS"
+  ttl     = 172800
+
+  records = [
+    "ns-1389.awsdns-45.org.",
+    "ns-186.awsdns-23.com.",
+    "ns-1887.awsdns-43.co.uk.",
+    "ns-865.awsdns-44.net.",
+  ]
+}
+
+resource "aws_route53_record" "rothwell-dev-soa" {
+  zone_id = aws_route53_zone.rothwell-dev-zone.zone_id
+  name    = "rothwell.dev"
+  type    = "SOA"
+  ttl     = 900
+
+  records = ["ns-865.awsdns-44.net.", "awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"]
+}
+
+resource "aws_route53_record" "rothwell-dev-a-record" {
+  zone_id = aws_route53_zone.rothwell-dev-zone.zone_id
+  name    = "rothwell.dev"
+  type    = "A"
+
+  alias {
+    name                   = aws_alb.playground_alb_load_balancer.dns_name
+    zone_id                = aws_route53_zone.rothwell-dev-zone.zone_id
+    evaluate_target_health = true
+  }
+}
